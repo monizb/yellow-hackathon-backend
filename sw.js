474bebcbf7bd9637b2678a9161d4f6ccd6ff7922
Monish Basaniwal <monish2.basaniwal@gmail.com>
2022-11-05 18:17:30 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,8 +2,8 @@ const Splitwise = require('splitwise')
 
 
 const sw = Splitwise({
-    consumerKey: process.env.consumerKey || "5iPo9JCJ4d5lH8d021J13m6d3FfpTgz5oXrDBhry",
-    consumerSecret: process.env.consumerSecret || "gOUTWPheEywjdNKw5Eu689Mk7PnuMjn2q96AUeo9"
+    consumerKey: process.env.consumerKey,
+    consumerSecret: process.env.consumerSecret
   })
 
 
